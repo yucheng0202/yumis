4e4ec0fde929257499da86d61f2218013fa4d3c4
yucheng0202 <113404853+yucheng0202@users.noreply.github.com>
2026-03-30 17:20:01 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ def index():
 	link += "<a href=/me>關於我</a><hr>"
 	link += "<a href=/welcome?u=宇呈&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
 	link += "<a href=/account>POST傳值</a><hr>"
+	link += "<a href=/math>次方根號</a><hr>"
 	return link
 
 @app.route("/mis")
