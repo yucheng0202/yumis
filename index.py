5ee8f31030b92f53e7bd606adc2d35716cbff0ba
yucheng0202 <113404853+yucheng0202@users.noreply.github.com>
2026-03-30 16:56:36 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -42,5 +42,8 @@ def account():
     else:
         return render_template("account.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
