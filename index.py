7dd18a2e118903bf0fb0fe8195608b262bf07a81
yucheng0202 <113404853+yucheng0202@users.noreply.github.com>
2026-04-09 11:41:05 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,30 +1,61 @@
 from flask import Flask, render_template,request
 from datetime import datetime
+
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# 判斷是在 Vercel 還是本地
+if os.path.exists('serviceAccountKey.json'):
+    # 本地環境：讀取檔案
+    cred = credentials.Certificate('serviceAccountKey.json')
+else:
+    # 雲端環境：從環境變數讀取 JSON 字串
+    firebase_config = os.getenv('FIREBASE_CONFIG')
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+
+firebase_admin.initialize_app(cred)
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	link = "<h1>歡迎進入徐宇呈的網站20260409</h1>"
-	link += "<a href=/mis>課程</a><hr>"
-	link += "<a href=/today>現在日期時間</a><hr>"
-	link += "<a href=/me>關於我</a><hr>"
-	link += "<a href=/welcome?u=宇呈&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
-	link += "<a href=/account>POST傳值</a><hr>"
-	link += "<a href=/math>次方根號</a><hr>"
-	return link
+    link = "<h1>歡迎進入徐宇呈的網站20260409</h1>"
+    link += "<a href=/mis>課程</a><hr>"
+    link += "<a href=/today>現在日期時間</a><hr>"
+    link += "<a href=/me>關於我</a><hr>"
+    link += "<a href=/welcome?u=宇呈&d=靜宜資管&c=資訊管理導論>Get傳值</a><hr>"
+    link += "<a href=/account>POST傳值</a><hr>"
+    link += "<a href=/math>次方根號</a><hr>"
+    link += "<a href=/read>讀取Firestore資料</a><hr>"
+    return link
+
+@app.route("/read")
+def read():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("靜宜資管2026B")    
+    docs = collection_ref.get()    
+    for doc in docs:         
+        Result += str(doc.to_dict()) + "<br>"    
+    return Result
+
 
 @app.route("/mis")
 def course():
-	return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
+    return "<h1>資訊管理導論</h1><a href=/>返回首頁</a>"
 
 @app.route("/today")
 def today():
-	now = datetime.now()
-	return render_template("today.html",datetime = str(now))
+    now = datetime.now()
+    return render_template("today.html",datetime = str(now))
 
 @app.route("/me")
 def me():
-	return render_template("about.html")
+    return render_template("about.html")
 
 @app.route("/welcome", methods=["GET"])
 def welcome():
