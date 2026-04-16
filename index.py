9e717c4e4695dbfbd5ad3cc26e829d3536b1ba66
yucheng0202 <113404853+yucheng0202@users.noreply.github.com>
2026-04-16 10:32:46 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -31,7 +31,26 @@ def index():
     link += "<a href=/account>POST傳值</a><hr>"
     link += "<a href=/math>次方根號</a><hr>"
     link += "<a href=/read>讀取Firestore資料</a><hr>"
+    link += "<a href=/read2>讀取Firestore資料(根據姓名關鍵字：楊)</a><hr>"
     return link
+
+@app.route("/read2")
+def read2():
+    Result = ""
+    keyword = "楊"
+    db = firestore.client()
+    collection_ref = db.collection("靜宜資管2026B")    
+    docs = collection_ref.get()
+    for doc in docs:
+        teacher = doc.to_dict()
+        if keyword in teacher["name"]:
+            Result += str(teacher) + "<br>"    
+    return Result
+
+    if Result == "":
+        Result +="抱歉,查無此關鍵字姓名之老師資料"
+    return Result
+
 
 @app.route("/read")
 def read():
