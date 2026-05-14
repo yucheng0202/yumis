c966a8d830124f1b9564f8c5e2e07208abc2f17c
yucheng0202 <113404853+yucheng0202@users.noreply.github.com>
2026-05-14 11:02:55 +0800

    webhook1

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime
 
 import os
@@ -43,6 +43,16 @@ def index():
     link += "<a href=/road2>查詢各縣市天氣預報</a><hr>"
     link += "<a href=/rate>本週新片</a><hr>"
     return link
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
 
 @app.route("/rate")
 def rate():
